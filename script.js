6447de79d9cec81cb67b96d7f9ea7b8d12687f99
swirly-way <s4m14z1l@gmail.com>
2024-12-05 12:00:07 +0000

    Advanced on pseudocode

---
+++ script.js
@@ -0,0 +1,32 @@
+/* 
+// Create all possible variables to initiate the game (playerName, playerChoice, computerChoice, currentScore and totalScore)
+// Computer choice variables are stored in an array.
+Call the function to run the game, initializing variables.
+Message: What’s your name? Store it in a variable and display it every round.
+Message: Would you like to play the game? If yes ask next question.
+Ask the player what is their choice using prompt().
+Store a move choice from a user (rock, paper, or scissors) in a variable as a string
+Randomly generate the computer's move and store that in a variable
+Compare the two moves using the rock, paper, scissors rules
+    Rock beats scissors
+    Scissors beats paper
+    Paper beats rock
+Update the scores
+Alert with message: ‘You chose R, computer chose P. You lose!’ with current score
+Alert the winner about the instant result and current score. (W,D,L should be included)
+Ask the player whether they want to play again
+Use loop for rounds
+
+*/
+
+
+
+let playerName = "";
+let playerChoice = "";
+let computerChoice = ["Rock", "Paper", "Scissors"];
+let currentScore = 0;
+let totalScore = 0;
+
+if(playerChoice === 'Rock' ){
+
+}
